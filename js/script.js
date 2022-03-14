6c1b6cc5084d5f4baee14b4f45029a824c436167
maggness <timderoller@gmail.com>
2022-03-14 14:27:24 +0100

    connection works

--- js/script.js
+++ js/script.js
@@ -29,12 +29,6 @@ function render(data) {
     const html = `
               <article>
                 <h2>${item.titles[0]}</h2>
-                <p>${
-                  item.summaries ? item.summaries[0] : "Geen samenvatting"
-                }</p>
-                <img src="${
-                  item.coverimages ? item.coverimages[1] : "Geen samenvatting"
-                }">
               </article>
             `
     main.insertAdjacentHTML("beforeend", html)
