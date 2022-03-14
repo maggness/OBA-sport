12084026b2925e4feaaf0768ac307fda0a2dd9b2
maggness <timderoller@gmail.com>
2022-03-14 14:16:18 +0100

    test

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-const endpoint = 'https://zoeken.oba.nl/api/v1/search/?q=';
-const query = 'tolkien';
-const key = '1e19898c87464e239192c8bfe422f280';
-const secret = '4289fec4e962a33118340c888699438d';
-const detail = 'Default';
-const url = `${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`;
+const getData = () => {
+  const cors = "https://cors-anywhere.herokuapp.com/";
+  const endpoint = "https://zoeken.oba.nl/api/v1/search/?q=";
+  const key = "1e19898c87464e239192c8bfe422f280";
+  const secret = "4289fec4e962a33118340c888699438d";
+  const detail = "Default";
+  const url = `${cors}${endpoint}&authorization=${key}&detaillevel=${detail}&output=json`;
+  const config = {
+    Authorization: `Bearer ${secret}`,
+  };
 
-const config = {
-  Authorization: `Bearer ${secret}`
+  fetch(url, config)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
-fetch(url, config)
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    render(data);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+getData();
